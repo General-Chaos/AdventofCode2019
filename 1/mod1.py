f39140cf083f7918bf3772f23393cdfbf128edf6
Nic McElroy <nicamcelroy@gmail.com>
2019-12-03 17:21:05 +0000

    pass tests

--- 1/mod1.py
+++ 1/mod1.py
@@ -1,7 +1,8 @@
 from typing import List
 
-def get_fuel(mass: int, recursive: bool = False) -> int :
-    initial = (mass//3) -2
+
+def get_fuel(mass: int, recursive: bool = False) -> int:
+    initial = (mass//3) - 2
     if initial > 0:
         yield initial
         if recursive:
@@ -9,11 +10,13 @@ def get_fuel(mass: int, recursive: bool = False) -> int :
     else:
         pass
 
+
 def get_fuel_list(masses: List[int], recursive: bool = False) -> List[int]:
     full_list = []
     for i in masses:
         full_list.extend(list(get_fuel(i, recursive)))
     return full_list
+
 
 def get_fuel_total(masses: List[int], recursive: bool = False) -> int:
     return sum(get_fuel_list(masses, recursive))
